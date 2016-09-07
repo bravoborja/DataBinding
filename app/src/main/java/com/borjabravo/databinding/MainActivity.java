808a472e071af8701cfca68f6553cc7835320b22
Borja Bravo <borja.bravoalvarez@gmail.com>
2016-09-07 09:07:14 +0200

    Observable model

--- app/src/main/java/com/borjabravo/databinding/MainActivity.java
+++ app/src/main/java/com/borjabravo/databinding/MainActivity.java
@@ -4,24 +4,23 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 import com.borjabravo.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("Borja", "Bravo", 24);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        user = new User("Borja", "Bravo", 24);
         binding.setUser(user);
     }
 
     public void onClickMe(View view) {
-        Toast.makeText(view.getContext(),
-                String.format(getString(R.string.click_me_info), binding.getUser().firstName, binding.getUser().age),
-                Toast.LENGTH_SHORT).show();
+        user.setFirstName("Juan");
+        user.setLastName("García");
+        user.setAge(35);
     }
 }
--- app/src/main/java/com/borjabravo/databinding/User.java
+++ app/src/main/java/com/borjabravo/databinding/User.java
@@ -1,15 +1,49 @@
 package com.borjabravo.databinding;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by borjabravo on 6/9/16.
  */
-public class User {
-    public String firstName;
-    public String lastName;
-    public int age;
+public class User extends BaseObservable {
+    private String firstName;
+    private String lastName;
+    private int age;
+
     public User(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    @Bindable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(com.borjabravo.databinding.BR.firstName);
+    }
+
+    @Bindable
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(com.borjabravo.databinding.BR.lastName);
+    }
+
+    @Bindable
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        notifyPropertyChanged(com.borjabravo.databinding.BR.age);
     }
 }
